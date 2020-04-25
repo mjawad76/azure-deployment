21b5260d13c7af7d7c29fe3789494af6902a6010
ahshahz <59127832+ahshahz@users.noreply.github.com>
2020-04-25 17:29:25 -0500

    Updated to use json need to parse json file.
    This commit has a prototype that actually gets the data

--- Parse Program/prayer.py
+++ Parse Program/prayer.py
@@ -3,7 +3,7 @@
 
 
 import requests #we access the website from this module
-
+import json
 
 
 
@@ -11,20 +11,22 @@ def prayer_data():
     latitude = 38.0043264 #test case form data
     longitude = -87.3627648 #test case form data
     timeformat = "24 hours" #test case form data
-    url = 'https://www.moonsighting.com/pray.php' #url where data is stored
-
+    #url = 'https://www.moonsighting.com/pray.php' #url where data is stored
+    url = 'https://www.moonsighting.com/time_json.php?year=2020&tz=America/Chicago&lat=38.0043264&lon=-87.3627648&method=0&both=false&time=0'
     form_data = {          #parameters to fill in table
-    'Latitude:': latitude,
-    'longitude:': longitude,
-    'time': '1',
+    'Latitude:': 38.0043264,
+    'longitude:': -87.3627648,
+    'time': 1,
     'Make Timetable': 'Make Timetable',
-    }
+    } #not needed url takes care of passing parameters
 
-    response = requests.post(url, form_data) #sending request with url and formdata
+    response = requests.get(url, data =form_data) #sending request with url and formdata change from post
 
     if response.status_code == 200: #we reached the website success!
-        print(response.text) #print what we got this will be trimmed
+        prayertimes = response.json() #convert to json
 
+       # print(response.text) #print what we got this will be trimmed
+        print(prayertimes)  #todo parse prayer times
     else:
         print("failed") #did not get data
 
