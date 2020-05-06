4f8312a680088d31277cc5ae87962b7ef24ed269
ahshahz <59127832+ahshahz@users.noreply.github.com>
2020-05-05 19:17:04 -0500

    Add files via upload

--- Parse Program/prayer.py
+++ Parse Program/prayer.py
@@ -1,49 +1,63 @@
 
 
 
+from tabulate import tabulate
 
 import requests #we access the website from this module
-import json
+
 
 
 
 def prayer_data():
-    latitude = 38.0043264 #test case form data
-    longitude = -87.3627648 #test case form data
-    timeformat = "24 hours" #test case form data
-    #url = 'https://www.moonsighting.com/pray.php' #url where data is stored
-    url = 'https://www.moonsighting.com/time_json.php?year=2020&tz=America/Chicago&lat=38.0043264&lon=-87.3627648&method=0&both=false&time=1'
-    form_data = {          #parameters to fill in table
-    'Latitude:': 38.0043264,
-    'longitude:': -87.3627648,
-    'time': 1,
-    'Make Timetable': 'Make Timetable',
-    } #not needed url takes care of passing parameters
+    year = '2020' #current year for url
+    timezone = 'America/Chicago' #must be in America/Chicago format
+    lat = '38.0043264' #lat in decimal format
+    lon = '-87.3627648' #lon in decimal format
+    method = '0' #calculation method for prayer
+    both = 'false' #if you want to see both asr prayer times depending on method selected
+    time = '1' #controls if am or pm or 24 hour clock format shown
+    dateStart = ' '
+    dateEnd = 'May 24'
 
-    response = requests.get(url, data =form_data) #sending request with url and formdata change from post
+    url = 'https://www.moonsighting.com/time_json.php?year=' + year + '&tz=' + timezone +'&lat=' + lat + '&lon=' + lon + '&method=' + method + '&both=' + both + '&time=' + time
+    #above line is parsed url with user data
+    response = requests.get(url) #sending request with url and formdata change from post
 
     if response.status_code == 200: #we reached the website success!
         prayertimes = response.json() #convert to json
 
-       # print(response.text) #print what we got this will be trimmed
-        #print(prayertimes)  #todo make a table with this data
-        #todo after making data make it so its location and methods can be changed and passed to api
-        #todo figure out a way to depoloy it on a website setting. 
-        #fajr = prayertimes['times'][0]  #gets first day list of prayers
-        #fajr = prayertimes['times'][0]['day'] #gets the date with the day
-        fajr = prayertimes['times'][0]['times']['fajr'] #this gets fajr prayer time
-        #timesfajir = [] #array of times for fajir
-        #timessunrise = []  # array of times for sunrise
-        dates = [] #array of dates
-        for x in prayertimes['times']:
-          print(x)
+        #todo figure out a way to depoloy it on a website setting.
+        #this prints the headers of the table
+        print("Date/Day    \tFajr     Sunrise     Dhur         Asr         Maghrib       Isha")
+        print("----------------------------------------------------------------------------------")
+        printvalues = True #if the user entered a date then this becomes false
+        if dateStart == " ":
+            printvalues = True
+        else:
+            printvalues = False
+        for x in prayertimes['times']: #loops through time data from the api
+           if printvalues: 
+                print(x['day'], "| ", x['times']['fajr'], "| ", x['times']['sunrise'], "| ", x['times']['dhuhr'], "| ",
+                      x['times']['asr'], "| ", x['times']['maghrib'], "| ", x['times']['isha'],
+                      "|")  # prints the date Jan 01 Wed
+                print("----------------------------------------------------------------------------------")
+           if len(dateStart) > 1 and dateStart in x['day']:
+               print(x['day'], "| " ,x['times']['fajr'], "| ", x['times']['sunrise'], "| ",x['times']['dhuhr'], "| ", x['times']['asr'], "| ", x['times']['maghrib'], "| ",x['times']['isha'], "|") #prints the date Jan 01 Wed
+               print("----------------------------------------------------------------------------------")
+               printvalues = True
+           if len(dateEnd) > 1 and dateEnd in x['day']:
+               print("date end: ", dateEnd)
+               print("x day: ", x['day'])
+               print(len(dateEnd))
+               print("break")
+               break
+
           #print(x, '->', prayertimes[x])
           #dates  = prayertimes['times'][0]['day']
 
         #print(fajr)
     else:
         print("failed") #did not get data
-
 
 
 
